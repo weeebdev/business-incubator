97f5ac9bac5b63f28f51135e46d3dc728fad01e0
Adil <mr.adil777@gmail.com>
2021-11-30 22:22:59 +0600

    feat(frontend): add prod and dev envs & Dockerfile

--- frontend/src/services/entries.js
+++ frontend/src/services/entries.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8080";
+const API = process.env.REACT_APP_API_URL;
 
 const getEntries = () => {
   return axios.get(`${API}/entries`).then((res) => res.data);
